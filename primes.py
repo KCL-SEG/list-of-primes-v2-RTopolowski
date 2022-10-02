f69ae5941f17987e0a49891012645b0c4d604ef9
Robert Topolowski <robert.topolowski@kcl.ac.uk>
2022-10-02 21:33:00 +0100

    Implement error handling

--- primes.py
+++ primes.py
@@ -2,5 +2,19 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes < 1:
+        raise ValueError("Number should be greater than 0.")
+
     list = []
+
+    current_number = 2
+    while len(list) < number_of_primes:
+        prime_flag = True
+        for i in range(2, int(current_number / 2) + 1):
+            if current_number % i == 0:
+                prime_flag = False
+        if prime_flag:
+            list.append(current_number)
+        current_number = current_number + 1
+
     return list
